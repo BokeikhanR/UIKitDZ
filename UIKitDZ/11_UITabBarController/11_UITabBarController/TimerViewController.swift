864133d477d2bc659aa278749d02406b796ed954
Ruslan <Rus.bokeykhan.97@bk.ru>
2021-07-02 23:44:25 +0600

    Сделал дз 11

---
+++ UIKitDZ/11_UITabBarController/11_UITabBarController/TimerViewController.swift
@@ -0,0 +1,80 @@
+//
+//  TimerViewController.swift
+//  11_UITabBarController
+//
+//  Created by admin on 02.07.2021.
+//
+
+import UIKit
+
+class TimerViewController: UIViewController, UIPickerViewDelegate {
+
+    @IBOutlet weak var pickerView: UIPickerView!
+    
+    
+    var hour: Int = 0
+    var minutes: Int = 0
+    var second: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        pickerView.delegate = self
+        pickerView.setValue(UIColor.white, forKey: "textColor")
+        
+    }
+
+
+}
+
+extension TimerViewController: UIDocumentPickerDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+    3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        switch component {
+        case 0:
+            return 25
+        case 1,2:
+            return 60
+        default:
+            return 0
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat{
+        return pickerView.frame.size.width/3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String {
+        switch component {
+        case 0:
+            return "\(row) ч"
+            case 1:
+                return "\(row) мин"
+        case 2:
+            return "\(row) сек"
+        default:
+            return ""
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        switch component {
+        case 0:
+            hour = row
+        case 1:
+            minutes = row
+        case 2:
+            second = row
+        default:
+            break;
+        }
+    }
+    
+//    func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
+//        let string = "\(row)"
+//        return NSAttributedString(string: string, attributes: [NSAttributedString.Key.foregroundColor: UIColor.white])
+//    }
+}
---
+++ UIKitDZ/11_UITabBarController/11_UITabBarController/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  11_UITabBarController
+//
+//  Created by admin on 02.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
