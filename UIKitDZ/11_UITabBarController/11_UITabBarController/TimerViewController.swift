d259307a6de51a5c998a225d9008bbebd56c4433
Ruslan <Rus.bokeykhan.97@bk.ru>
2021-07-03 15:41:04 +0600

    Исправил замечание

--- UIKitDZ/11_UITabBarController/11_UITabBarController/TimerViewController.swift
+++ UIKitDZ/11_UITabBarController/11_UITabBarController/TimerViewController.swift
@@ -7,45 +7,43 @@
 
 import UIKit
   
-final class TimerViewController: UIViewController, UIPickerViewDelegate {
+//MARK: View Configuration
 
-    @IBOutlet weak var pickerView: UIPickerView!
+
+final class TimerViewController: UIViewController {
+
+    //Picker View Outlet
     
+    @IBOutlet var pickerView: UIPickerView!
     
-     private var hour: Int = 0
-     private var minutes: Int = 0
-     private var second: Int = 0
+    // Properties
+    
+     private var hour = 0
+     private var minutes = 0
+     private var second = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         pickerView.delegate = self
         pickerView.setValue(UIColor.white, forKey: "textColor")
-        
     }
-
-
 }
 
-extension TimerViewController: UIDocumentPickerDelegate, UIPickerViewDataSource {
+//MARK: Extension
+
+//Component Number
+extension TimerViewController: UIPickerViewDataSource {
     
     func numberOfComponents(in pickerView: UIPickerView) -> Int {
         3
     }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        switch component {
-        case 0:
-            return 25
-        case 1,2:
-            return 60
-        default:
-            return 0
-        }
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat{
-        return pickerView.frame.size.width/3
-    }
+}
+
+//Component Views
+
+extension TimerViewController: UIPickerViewDelegate{
+
+    //View Names
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String {
         switch component {
@@ -60,16 +58,16 @@ extension TimerViewController: UIDocumentPickerDelegate, UIPickerViewDataSource 
         }
     }
     
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    //View times
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         switch component {
         case 0:
-            hour = row
-        case 1:
-            minutes = row
-        case 2:
-            second = row
+            return 25
+        case 1,2:
+            return 60
         default:
-            break;
+            return 0
         }
     }
 }
+
