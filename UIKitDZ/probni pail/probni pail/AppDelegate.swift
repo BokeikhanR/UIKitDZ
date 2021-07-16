fe0b22ccba173e186981ca40d7a93850d0957d0e
Ruslan <Rus.bokeykhan.97@bk.ru>
2021-07-17 03:09:53 +0600

    Domawni zadanie 23 sdelan

---
+++ UIKitDZ/probni pail/probni pail/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  probni pail
+//
+//  Created by admin on 16.07.2021.
+//
+
+import UIKit
+
+@main
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+
+}
+
